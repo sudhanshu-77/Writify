fdddf8092ba514c395933ab9393a946cf6c839e2
Sudhanshu Tripathi <97780892+sudhanshu-77@users.noreply.github.com>
2024-09-08 11:55:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': 
       {
-        target: 'https://review01-backend.onrender.com',
+        target: 'https://writify-blogging-app.onrender.com',
         changeOrigin: true,
         secure: true,
       },
